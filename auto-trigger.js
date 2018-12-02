bff4bc5482408e4d568462bc2b059de6a650141d
sjindal94 <shubhamjindal_1994@yahoo.com>
2018-12-01 21:36:18 -0500

    refactor

--- auto-trigger.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-    navigator.mediaDevices.getUserMedia({ audio: true })
-    .catch(function() {
-        chrome.tabs.create({
-            url: chrome.extension.getURL("options.html"),
-            selected: true
-        })
-    });
-}, 100);
